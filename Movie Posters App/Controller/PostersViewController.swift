ae073e63da4ab60326695abb59a40f2a9d74aabc
SebaKrk <s.sciuba@gmail.com>
2020-11-11 22:45:13 +0100

    add navigation controller

---
+++ Movie Posters App/Controller/PostersViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PostersViewController.swift
+//  Movie Posters App
+//
+//  Created by Sebastian Sciuba on 11/11/2020.
+//
+
+import UIKit
+
+class PostersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
