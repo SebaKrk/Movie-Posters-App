fe13b101ba020fe980890c16f42758f33269a518
SebaKrk <s.sciuba@gmail.com>
2020-11-12 19:23:06 +0100

    create a plot label and set programmatically a text properties

--- Movie Posters App/Controller/MovieDetailsViewController.swift
+++ Movie Posters App/Controller/MovieDetailsViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var plotLabel: UILabel!
+    
+    var posterManager = PostersManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black
+        plotLabel.textColor = .white
+        plotLabel.numberOfLines = 0
+        plotLabel.text = "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader."
+        
 
     }
     
