1239be60c038d0653170fa0264f25455937bf6dc
SebaKrk <s.sciuba@gmail.com>
2020-11-12 07:23:51 +0100

    added images and buttons in posterViewController

--- Movie Posters App/Controller/ViewController.swift
+++ Movie Posters App/Controller/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "Please enter title of movie", attributes: [NSAttributedString.Key.foregroundColor:UIColor.white])
+        
+        
     }
 
-
+    @IBAction func searchButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
 
